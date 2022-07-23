4111f167fc55db561db72f7d3b3148c71a0e2fcc
lulubit <lucasurrutia21@gmail.com>
2022-07-23 11:50:42 -0700

    Accounting Welcome

---
+++ AccountingWelcome.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Accounting
+{
+    static void main()
+    {
+        Console.WriteLine("Welcome To Acme Accounting Systems");
+        Console.WriteLine("Remember, we're accounting on you!");
+        Console.Readline();
+    }
+}
