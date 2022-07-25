7aab8b5b4a58bd7f2854b5d8e695400dcb288cbf
lulubit <lucasurrutia21@gmail.com>
2022-07-24 23:12:45 -0700

    IncomeComparison Assigment

---
+++ IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("What is your hourly rate?");
+            int rate = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many hours do you work per week?");
+            int hrsPerWeek = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("What is your hourly rate?");
+            int rate2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many hours do you work per week?");
+            int hrsPerWeek2 = Convert.ToInt32(Console.ReadLine());
+
+            int salary1 = rate * hrsPerWeek * 52;
+            int salary2 = rate2 * hrsPerWeek2 * 52;
+            Console.WriteLine("Annual salary of Person 1: " +
+                salary1);
+
+            Console.WriteLine("Annual salary of Person 2: " +
+                salary2);
+
+            Console.WriteLine("Person 1 make more money than Person 2?");
+            bool compare = salary1 > salary2;
+            Console.WriteLine(compare);
+            Console.ReadLine();
+        }
+    }
+}
