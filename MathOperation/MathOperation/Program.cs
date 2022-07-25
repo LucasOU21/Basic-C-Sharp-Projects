89b47b7818e40b7b476369f01dc95a2a700f30bf
lulubit <lucasurrutia21@gmail.com>
2022-07-24 22:15:46 -0700

    Math Operations Assisgment

---
+++ MathOperation/MathOperation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick any number under 10,000,000: ");
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+            int multiply = num * 50;
+            Console.WriteLine(multiply);
+            Console.ReadLine();
+
+            Console.WriteLine("Now Pick any number between 0 and 1000: ");
+            string newInput = Console.ReadLine();
+            int newNum = Convert.ToInt32(newInput);
+            int addition = newNum + 25;
+            Console.WriteLine(addition);
+            Console.ReadLine();
+
+            Console.WriteLine(" Once again Pick any number between 0 and 1000: ");
+            string secondInput = Console.ReadLine();
+            int secondNum = Convert.ToInt32(secondInput);
+            double division = secondNum / 12.5;
+            Console.WriteLine(division);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick any number under 10,000,000: ");
+            string compareInput = Console.ReadLine();
+            int numInput = Convert.ToInt32(compareInput);
+            bool greaterThan = numInput > 50;
+            Console.WriteLine(greaterThan);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick another number under 10,000,000: ");
+            string modInput = Console.ReadLine();
+            int modulInput = Convert.ToInt32(modInput);
+            int remainder = modulInput % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
