50e30791480358ef81e6a9b861fa180f08708ace
lulubit <lucasurrutia21@gmail.com>
2022-07-30 22:24:51 -0700

    StringAssignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Uno = " Ciao, ";
+            string Dos = "how are you? ";
+            string Tres = "My name is Pablo ";
+
+            Dos = Dos.ToUpper();
+            Console.WriteLine(Uno + Dos + Tres);
+            Console.ReadLine();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Pablo");
+            sb.Append("\nit is very nice to meet you!");
+            sb.Append("\nPlease don't be shy.");
+            sb.Append("\nTell me more about yourself?");
+            sb.Append("\nWell thank you for your time!");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
